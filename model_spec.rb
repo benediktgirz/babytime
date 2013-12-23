df4af9f5912f953aac41204d01de78f2c6d7b257
benediktgirz <benedikt.girz@gmail.com>
2013-12-23 01:11:34 -0800

    readme and pseudo user model tests

---
+++ model_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+  describe User do 
+
+    xit "should have an email"
+    xit "should have a confirmed email"
+    xit "should have a password"
+    xit "should have a confirmed password"
+    xit "has many message alerts"
+  end
