ada8cee6a4386bb84b9f2b4b1b451398a672d637
benediktgirz <benedikt.girz@gmail.com>
2013-12-26 12:12:40 +0100

    clean up of root

---
+++ babytime/model_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+  describe User do 
+
+    xit "should have an email"
+    xit "should have a confirmed email"
+    xit "should have a password"
+    xit "should have a confirmed password"
+    xit "has many message alerts"
+  end
+
+  describe Message do
+      xit "should have users"
+      
+  end
