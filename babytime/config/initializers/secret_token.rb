e6c8f113181eeb4384224be067c186ee80a14482
benediktgirz <benedikt.girz@gmail.com>
2013-12-26 04:17:52 +0100

    rails app kicked off

---
+++ babytime/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babytime::Application.config.secret_key_base = 'c1623db441bd94a8f31f5c57dac10983eb1074ba4b6f08fd86c9a97885359fd4828a082ce443509772d9cdcdbb4a37022d350c3c0cafba41871ebeb740587276'
---
+++ babytime/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Babytime::Application.config.session_store :cookie_store, key: '_babytime_session'
