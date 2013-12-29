50d08a9444d5c22d28050708670a399b55288c10
benediktgirz <benedikt.girz@gmail.com>
2013-12-29 19:30:25 +0100

    green uniquenss of message id

--- app/models/messagetemplate.rb
+++ app/models/messagetemplate.rb
@@ -1,4 +1,4 @@
 class Messagetemplate < ActiveRecord::Base
   validates_presence_of :message_template_id, :content, :subject, :send_next, :last_sent
-  #validates_uniqueness_of :message_template_id, 
+  validates_uniqueness_of :message_template_id
 end
