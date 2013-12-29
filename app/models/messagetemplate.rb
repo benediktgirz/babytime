a274f78efe7a7a540c86d0e16e2630dd1479688e
benediktgirz <benedikt.girz@gmail.com>
2013-12-29 19:29:25 +0100

    refactored model test, plus red uniquness of message

--- app/models/messagetemplate.rb
+++ app/models/messagetemplate.rb
@@ -1,8 +1,4 @@
 class Messagetemplate < ActiveRecord::Base
-  validates_presence_of :message_template_id
-  validates_presence_of :content
-  validates_presence_of :subject
-  validates_presence_of :send_next
-  validates_presence_of :last_sent
-
+  validates_presence_of :message_template_id, :content, :subject, :send_next, :last_sent
+  #validates_uniqueness_of :message_template_id, 
 end
--- spec/models/messagetemplate_spec.rb
+++ spec/models/messagetemplate_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Messagetemplate do
   it { should validate_presence_of(:message_template_id)}
+  it { should validate_uniqueness_of(:message_template_id)}
   it { should validate_presence_of(:content)}
   it { should validate_presence_of(:subject)}
   it { should validate_presence_of(:send_next)}
