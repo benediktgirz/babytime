bf3c7ed4d120f975e1fe6e616da553b4f63462bd
benediktgirz <benedikt.girz@gmail.com>
2013-12-29 17:30:43 +0100

    red message template is missing its 5 properties

--- spec/models/messagetemplate_spec.rb
+++ spec/models/messagetemplate_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Messagetemplate do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:message_template_id)}
+  it { should validate_presence_of(:content)}
+  it { should validate_presence_of(:subject)}
+  it { should validate_presence_of(:send_next)}
+  it { should validate_presence_of(:last_sent)}
 end
