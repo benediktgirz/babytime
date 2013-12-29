4b64ea95cc1b5b4ef702220e462ec1dc391aba87
benediktgirz <benedikt.girz@gmail.com>
2013-12-29 17:17:07 +0100

    red password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 describe User do
   it { should validate_presence_of(:user_id)}
   it { should validate_presence_of(:email)}
-  xit { should validate_presence_of(:user_id)}
+  it { should validate_presence_of(:password)}
 end
