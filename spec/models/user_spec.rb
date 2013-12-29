149d4d993b8462790fbda62fa0dd8189e9238a80
benediktgirz <benedikt.girz@gmail.com>
2013-12-29 17:14:24 +0100

    red email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe User do
   it { should validate_presence_of(:user_id)}
+  it { should validate_presence_of(:email)}
+  xit { should validate_presence_of(:user_id)}
 end
