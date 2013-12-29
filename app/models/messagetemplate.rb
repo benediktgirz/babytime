ddd3789f15748d1396e0305d90c27927140bb8a0
benediktgirz <benedikt.girz@gmail.com>
2013-12-29 17:37:26 +0100

    green message template properties

--- app/models/messagetemplate.rb
+++ app/models/messagetemplate.rb
@@ -1,2 +1,8 @@
 class Messagetemplate < ActiveRecord::Base
+  validates_presence_of :message_template_id
+  validates_presence_of :content
+  validates_presence_of :subject
+  validates_presence_of :send_next
+  validates_presence_of :last_sent
+
 end
