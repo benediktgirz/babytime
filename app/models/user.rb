609d9201878e90188493bffbf287393909d50d41
benediktgirz <benedikt.girz@gmail.com>
2013-12-29 19:38:46 +0100

    green uniqueness user mail, id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   validates_presence_of :user_id, :email, :password
+  validates_uniqueness_of :user_id, :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,6 @@ describe User do
   it { should validate_presence_of(:user_id)}
   it { should validate_uniqueness_of(:user_id)}
   it { should validate_presence_of(:email)}
-  it { should validate_uniqueness_of(:emai)}
+  it { should validate_uniqueness_of(:email)}
   it { should validate_presence_of(:password)}
 end
