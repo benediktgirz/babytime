5e831d1842cb34d3fddc6000c94525f85c908fa2
benediktgirz <benedikt.girz@gmail.com>
2014-01-01 22:50:04 +0100

    red perioddata id, date

--- spec/models/perioddata_spec.rb
+++ spec/models/perioddata_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Perioddata do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:period_id)}
+  it { should validate_presence_of(:date)}
 end
