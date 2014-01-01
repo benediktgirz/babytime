1c866107f00b0a44160de03c3168b4fb9259645d
benediktgirz <benedikt.girz@gmail.com>
2014-01-01 22:31:47 +0100

    red uniquness email and id message recipi

--- spec/models/messagerecipient_spec.rb
+++ spec/models/messagerecipient_spec.rb
@@ -4,4 +4,6 @@ describe Messagerecipient do
   it { should validate_presence_of(:message_template_id)}
   it { should validate_presence_of(:email)}
   it { should validate_presence_of(:name)}
+  it { should validate_uniqueness_of(:message_template_id)}
+  it { should validate_uniqueness_of(:email)}
 end
