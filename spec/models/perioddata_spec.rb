7dc049c218305a62f5d85992bbdee35456a35613
benediktgirz <benedikt.girz@gmail.com>
2014-01-01 22:53:40 +0100

    red uniqueness of period data id

--- spec/models/perioddata_spec.rb
+++ spec/models/perioddata_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 describe Perioddata do
   it { should validate_presence_of(:period_id)}
   it { should validate_presence_of(:date)}
+  it { should validate_uniqueness_of(:period_id)}
 end
