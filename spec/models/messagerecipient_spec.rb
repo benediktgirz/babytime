e608faa2eca169cbffa90370f94daf24df365aae
benediktgirz <benedikt.girz@gmail.com>
2014-01-01 22:27:23 +0100

    red messaagerecipi id, name, email

--- spec/models/messagerecipient_spec.rb
+++ spec/models/messagerecipient_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Messagerecipient do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:message_template_id)}
+  it { should validate_presence_of(:email)}
+  it { should validate_presence_of(:name)}
 end
