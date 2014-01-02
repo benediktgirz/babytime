f08da62f88c62101e2f69b675598f894ffe996e1
benediktgirz <benedikt.girz@gmail.com>
2014-01-02 08:46:14 +0100

    green presence of unique url

--- app/models/passwordresetlink.rb
+++ app/models/passwordresetlink.rb
@@ -1,3 +1,4 @@
 class Passwordresetlink < ActiveRecord::Base
   validates_uniqueness_of :unique_url
+  validates_presence_of :unique_url
 end
--- spec/models/passwordresetlink_spec.rb
+++ spec/models/passwordresetlink_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Passwordresetlink do
   it { should validate_uniqueness_of(:unique_url)}
+  it { should validate_presence_of(:unique_url)}
 end
