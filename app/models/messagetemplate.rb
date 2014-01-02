4287be77a9c2a9462964f6164ebbab4892754f13
benediktgirz <benedikt.girz@gmail.com>
2014-01-02 09:06:55 +0100

    all models are fully available and tested

--- app/models/messagetemplate.rb
+++ app/models/messagetemplate.rb
@@ -1,5 +1,6 @@
 class Messagetemplate < ActiveRecord::Base
   belongs_to :user
+  has_many :messagerecipients
   validates_presence_of :message_template_id, :content, :subject, :send_next, :last_sent
   validates_uniqueness_of :message_template_id
 end
